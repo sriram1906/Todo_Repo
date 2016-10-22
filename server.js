c5dcb8b8febb3aec593c45624ae45eae3c6bd150
Sriram <sriram@Srirams-MacBook-Pro.local>
2016-10-22 19:16:45 -0400

    Node_Repo

--- server.js
+++ server.js
@@ -5,9 +5,33 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+var todos = [{
+    id : 1,
+    Description : "Learn node js",
+    Completed : false
+}, {
+        id : 2,
+        Description : "health",
+        Completed : false
+    },{
+    id : 3,
+    Description : "drink boost",
+    Completed : true
+}]
+
 app.get('/', function(req,res)
 {
     res.send("Todo API Root");
+});
+
+app.get('/todos', function(req,res)
+{
+    res.json(todos);
+});
+
+app.get('/todos/:id', function(req,res)
+{
+    res.json("Asking for todo with id of " + req.params.id);
 });
 
 
